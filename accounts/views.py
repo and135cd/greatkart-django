1aba42d6d232fca802db8b2784977853f29b7109
Gabriela Ovalle Motta <gaby26@umes.edu.gt>
2023-10-21 22:42:44 -0600

    carrito de compras actualizado

--- accounts/views.py
+++ accounts/views.py
@@ -13,6 +13,10 @@ from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
+import requests
+
 
 def register(request):
     if request.method == 'POST':
@@ -50,6 +54,7 @@ def register(request):
     return render(request, 'accounts/register.html', context)
 
 
+
 def login(request):
     if request.method == 'POST':
         email = request.POST['email']
@@ -58,9 +63,56 @@ def login(request):
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
+            try:
+                cart=Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item=CartItem.objects.filter(cart=cart)
+
+                    #Obteniendo la variacion del producto del carrito id
+                    product_variation=[]
+                    for item in cart_item:
+                        variation=item.variations.all()
+                        product_variation.append(list(variation))
+
+                    #obtener los porductos del carrito del usuario para accesar a sus variaciones de producto
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    #se verifica el index de la variacion del producto de la lista con los que existen
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index=ex_var_list.index(pr)
+                            item_id=id[index]
+                            item=CartItem.objects.get(id=item_id)
+                            item.quantity+=1
+                            item.user=user
+                            item.save()
+                        else:
+                            cart_item=CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user=user
+                                item.save()                   
+            except:
+                pass
             auth.login(request, user)
             messages.success(request, 'Ahora estás logueado.')
-            return redirect('dashboard')
+            url=request.META.get('HTTP_REFERER')
+            try:
+                query=requests.utils.urlparse(url).query
+                #next=/cart/checkout
+                params=dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage=params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
+            
         else:
             messages.error(request, 'Datos incorrectos')
             return redirect('login')
